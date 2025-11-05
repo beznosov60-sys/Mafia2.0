d1b436f51d18b64d489a17d912a0e1afdd30fdfa
Vadim <beznosov60@gmail.com>
2025-11-06 01:06:11 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,4 @@
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
-import path from 'path';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@scss': path.resolve(__dirname, './src/scss'),
-      '@bootstrap': path.resolve(__dirname, './node_modules/bootstrap'),
-    },
-  },
-  plugins: [ViteImageOptimizer()],
-});
-
-=======
 import vue from '@vitejs/plugin-vue';
 import path from 'node:path';
 
@@ -26,4 +10,3 @@ export default defineConfig({
     }
   }
 });
->>>>>>> 4aeb9276f7e7298a19e2a614b311b9d8e06723f1
